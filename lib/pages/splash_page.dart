ddbdc227e4e06358358d18dbfff1def7eb915e46
fakhri ahnaf dhia <fakhri_ahnaf01@yahoo.co.id>
2021-06-23 17:20:27 +0700

    app commit

---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+
+import 'package:AStore/theme.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    
+    Timer(
+      Duration(seconds: 3),
+      () => Navigator.pushNamed(context, '/sign-in'),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor1,
+      body: Center(
+        child: Container(
+          width: 130,
+          height: 150,
+          decoration: BoxDecoration(
+              image: DecorationImage(image: AssetImage('assets/logo.png'))),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/theme.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+double defaultMargin = 30.0;
+
+Color primaryColor = Color(0xff2C96F1);
+Color secondaryColor = Color(0xff2C96F1);
+Color alertColor = Color(0xffed6363);
+Color priceColor = Color(0xff38abbe);
+Color backgroundColor1 = Color(0xff080C20);
+Color backgroundColor2 = Color(0xFF1E2338);
+Color backgroundColor3 = Color(0xFF13172C);
+Color primaryTextColor = Color(0xFFF1F0F2);
+Color secondaryTextColor = Color(0xFFC4C4C4);
+Color subtitleTextColor = Color(0xFFC6C6C6);
+
+TextStyle primaryTextStyle = GoogleFonts.poppins(
+  color: primaryTextColor,
+);
+TextStyle secondaryTextStyle = GoogleFonts.poppins(
+  color: secondaryTextColor,
+);
+TextStyle priceTextStyle = GoogleFonts.poppins(
+  color: priceColor,
+);
+TextStyle subtitleTextStyle = GoogleFonts.poppins(color: subtitleTextColor);
+TextStyle blueTextStyle = GoogleFonts.poppins(color: primaryColor);
+
+FontWeight light = FontWeight.w300;
+FontWeight reguler = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
